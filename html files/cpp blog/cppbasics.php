8f753620657b2928178de3c0b83b2a314fc6f9d8
adityavsm-is19 <adityavsm.is19@bmsce.ac.in>
2021-08-04 22:03:54 +0530

    minor update

--- html files/cpp blog/cppbasics.php
+++ html files/cpp blog/cppbasics.php
@@ -60,30 +60,7 @@
                 <p>
                     <h1>C++ Basics</h1> <br><br>
 
-            <form method="POST" onsubmit="saved(event)">
-                <input type="submit" class="save btn btn-primary" value="Save for later"></button>
-            </form>
-
-            <script type="text/javascript">
-            function saved(event){     
-                event.preventDefault();                 
-                <?php
-                    $name = "C++ Basics";
-                    $query1 = "SELECT name FROM bokmark WHERE name='".$name."'";
-                    $result2 = mysqli_query($con,$query1);
-
-                    if(mysqli_num_rows($result2)==0 && $_SERVER['REQUEST_METHOD'] == "POST"){
-                        $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; 
-                        $query = "INSERT INTO bokmark(user_id, bookmark_url,name) VALUES('$id','$url','$name');";
-                        mysqli_query($con,$query);
-                        alert("Page saved successfully"); 	
-                        header("Location: ../../bookmark.php");                                       
-                    }else{
-                        alert("Page already saved");
-                    }                             
-                ?>
             
-        </script> 
                     <p>
                         C++ is a statically typed, compiled, general-purpose, case-sensitive, free-form programming language that supports procedural, object-oriented, and generic programming.
                     </p>
